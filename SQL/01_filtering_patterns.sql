f32fc72556b7c03a0b2214c90e296e5913badcfd
Sajal Bhattarai <codersajal@gmail.com>
2026-02-23 14:54:14 -0500

    Add SQL queries for filtering patterns

--- SQL/01_filtering_patterns.sql
+++ SQL/01_filtering_patterns.sql
@@ -1,1 +1,15 @@
+-- Problem: Revising the Select Query I
+-- Source: HackerRank
+-- Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- Pattern: Conditional Row Filtering
 
+SELECT * 
+FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+
+-- Problem: Revising the Select Query II
+-- Source: HackerRank
+-- Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- Pattern: Conditional Row Filtering
+
+SELECT NAME 
+FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
